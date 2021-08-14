8770d305ad215127fcc7ad5e245d01850e896157
hiroki917 <kensmith_5276_zemaitis@yahoo.co.jp>
2021-08-14 19:10:02 +0900

    再再提出

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,8 @@
 class UsersController < ApplicationController
   def show
+    
     @user = User.find(params[:id])
-    @prototype = @user.prototypes
+    @prototypes = @user.prototypes
+    # @prototypes = Prototype.all
   end
 end
